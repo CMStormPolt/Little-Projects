483b03f26182f2b7e13ddc5fdb276e5c361f3438
alexander_bg <cmstormpolt@gmail.com>
2016-10-18 12:47:47 +0300

    Import

---
+++ Simple Shop App/task/task-2.js
@@ -0,0 +1,148 @@
+/* globals module */
+
+"use strict";
+
+function solve(){
+    let costSum = 0,
+        typesToReturn = [];
+    class Product{
+        constructor(productType,name,price){
+     
+                this._productType = productType;   
+                 this._name = name;  
+                 this._price = price;
+            
+        }
+       get productType(){
+           return this._productType;
+       } 
+       set productType(newType){
+           if(typeof newType == 'string'){
+               this._productType = newType
+           }
+       }
+       get name(){
+           return this._name;
+       } 
+       set name(newName){
+           if(typeof newName == 'string'){
+               this._name = newName;
+           }
+       }
+       get price(){
+           return this._price;
+       } 
+       set price(newPrice){
+           if(typeof newPrice == 'number'){
+               this._price = newPrice;
+           }
+       }
+    }
+
+    class ShoppingCart {
+      constructor(){
+          this._products = [];      
+    }
+    get products(){
+        return this._products;
+    }
+    add(product){
+        this._products.push(product)
+        return this;
+    }
+    remove(product){
+        let len = this._products.length;  
+        for(let i = 0; i < len; i += 1){
+            if(this.products[i].name == product.name && this.products[i].productType == product.productType &&
+            this.products[i].price == product.price){
+                this._products.splice(i,1)
+                return;
+            }
+        }
+        throw 'I am throwing you'
+    }
+    showCost(){
+    let len = this._products.length;  
+        for(let i = 0; i < len; i += 1){
+           costSum += this._products[i].price;
+        }
+        return costSum;
+    }
+    showProductTypes(){
+        let len = this._products.length;
+          for(let i = 0; i < len; i += 1){
+              let toBeAdded = true;
+    
+              for(let j = 0; j < typesToReturn.length; j += 1){
+                  if(this._products[i].productType == typesToReturn[j]){
+                      console.log(this.products[i].productType);
+                      toBeAdded = false
+                  }
+              }
+              if(toBeAdded){
+                  typesToReturn.push(this._products[i].productType)
+              }
+          }
+          typesToReturn.sort(function(a,b){
+              
+                if(a < b) {
+                    return - 1
+                } else if(a > b){
+                    return 1
+                }
+              return 0
+          })
+          return typesToReturn;
+    }
+    getInfo(){
+      let products = [],
+          totalPrice = 0,
+          len = this.products.length;
+          for(let i = 0; i < len; i += 1){
+             let toBeAdded = true,
+                 elToBeAdded = {
+                     name: 'notYet',
+                     totalPrice: 0,
+                     quanity: 0
+                 },
+                 elToAdd;
+             for(let j = 0; j < products.length; j += 1){
+                 if(this.products[i].name == products[j].name){
+                     toBeAdded = false;
+                     elToAdd = products[j];
+                     break;
+                 }
+             }
+             if(toBeAdded){
+                elToBeAdded.name = this.products[i].name;
+                elToBeAdded.totalPrice = this.products[i].price;
+                elToBeAdded.quanity = 1;
+                totalPrice += this.products[i].price;
+                products.push(elToBeAdded);
+
+             } else {
+                elToAdd.totalPrice += this.products[i].price;
+                elToAdd.quanity += 1; 
+                totalPrice += this.products[i].price;
+             }
+          }
+          return {
+              totalPrice: totalPrice,
+              products: products
+          }
+    }
+
+    
+    }
+    return {
+        Product, ShoppingCart
+    };
+}
+let constr = solve(),
+    sc = new constr.ShoppingCart();
+    let p1 = new constr.Product('shit1','shitty',1),
+        p2 = new constr.Product('bshit2','shitty2',2),
+        p3 = new constr.Product('bshit2','shittu',4);
+    sc.add(p1).add(p2).add(p3);
+    console.log(sc.getInfo());
+module.exports = solve
